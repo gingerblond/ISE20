a347452da70cf0ab7698a6723fea8a50d52173dc
Sladjana Bujadnjak <a01469339@unet.univie.ac.at>
2021-01-07 14:58:51 +0100

    Employee entiry, service and controller

---
+++ hotel/api/src/main/java/com/example/hotel/controller/EmployeeController.java
@@ -0,0 +1,57 @@
+package com.example.hotel.controller;
+
+
+import com.example.hotel.entity.Employee;
+import com.example.hotel.service.EmployeeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeService service;
+
+    /**
+     * Save new customer
+     */
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @PostMapping("addEmployee")
+    public Employee addCustomer(@RequestBody Employee employee){
+        return service.addEmployee(employee);
+    }
+
+    /**
+     * List all customers
+     */
+    @GetMapping("employees")
+    public List<Employee> findAllCustomers() {
+        return service.getEmployees();
+    }
+
+    /**
+     * Search for customer by id
+     */
+    @GetMapping("employeeById/{id}")
+    public Employee findCustomerById(@PathVariable int id) {
+        return service.getEmployeeById(id);
+    }
+
+    /**
+     * Update customer details
+     */
+    @PutMapping("updateEmployee")
+    public Employee updateCustomer(@RequestBody Employee employee) {
+        return service.updateEmployee(employee);
+    }
+
+    /**
+     * Delete customer by id
+     */
+    @DeleteMapping("deleteEmployee/{id}")
+    public String deleteCustomer(@PathVariable int id) {
+        return service.deleteEmployee(id);
+    }
+}
